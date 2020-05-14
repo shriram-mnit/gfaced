8c41e14bc6f66c35e2babaee1057ef3746dba5b2
shriram-mnit <shriram969460@gmail.com>
2020-05-14 11:11:54 +0000

    build

---
+++ client/src/components/FaceRecognition/Box.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+//box.forEach(() => 
+        //{
+       // <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      //  })
+const Box =({Img})=>
+{
+ return (<div >
+{
+   Img
+}
+</div>
+    )
+}
+export default Box;
