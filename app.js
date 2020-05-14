484eabda6f04bddc94b1f46ab2a134df4dfd0937
shriram-mnit <shriram969460@gmail.com>
2020-05-14 14:09:22 +0000

    add

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express =require('express')
 const mongoose = require('mongoose')
-const {MONGOURI}= require('./keys')
 const cors= require('cors')
 const app =express()
-const PORT =5000
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
 
 app.use(cors())
 
@@ -23,7 +23,13 @@ require('./models/user')
 app.use(express.json())
 
 app.use(require('./routes/auth'))
-
-
-
-app.listen(5000)
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
