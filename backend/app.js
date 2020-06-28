f28917e41155da3c6acefbcac5c3d88dffa3cfe9
shriram-mnit <shriram969460@gmail.com>
2020-06-28 11:41:17 +0000

    some errr is fixed

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const mongoose = require('mongoose')
 const cors= require('cors')
+const path = require('path')
 const app =express()
 const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
@@ -10,6 +11,10 @@ app.use(cors())
 mongoose.connect(MONGOURI,{
 	 useNewUrlParser:true,
 	useUnifiedTopology:true
+})
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
 })
 
 mongoose.connection.on('connected',()=>{
