861dbf83a37437af342545ec285abe7067e180c8
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2020-09-15 16:33:22 +0530

    remove cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,9 @@
 const express =require('express')
 const mongoose = require('mongoose')
-const cors= require('cors')
 const path = require('path')
 const app =express()
 const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
-
-app.use(cors())
-
 mongoose.connect(MONGOURI,{
 	 useNewUrlParser:true,
 	useUnifiedTopology:true
