1b8df2de68da94c911dc5997284f3e2161c330df
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2020-09-15 14:16:54 +0530

    Change route

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('/signup', {
+    fetch('https://localhost:5000/signup', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
